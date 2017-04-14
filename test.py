6c93418ba2277b686d996553dad1ac77f48de233
Xin Wang <wangxin94@163.com>
2017-04-14 10:37:30 -0400

    hstack and vstack

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import numpy as np
-a = np.array([1, 2, 3])
-b = np.array((1, 2, 3))
+##
+# hstack and vstack concatenate two arrays
+a = np.array([[1, 2, 3],
+                [1, 2, 3]])
+b = np.array([[4, 5, 6],
+                [7, 8, 9]])
+print 'a = \n', a.shape
+print 'b = \n', b.shape
+c = np.hstack((a, b))
+d = np.vstack((a, b))
+print 'c = \n', c
+print 'd = \n', d
