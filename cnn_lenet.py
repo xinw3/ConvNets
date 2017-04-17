9c5aadb913b092691d4c9b7215508a544a7ea323
Xin Wang <wangxin94@163.com>
2017-04-17 17:19:21 -0400

    can show figures

--- cnn_lenet.py
+++ cnn_lenet.py
@@ -243,25 +243,51 @@ def conv_net(params, layers, data, labels):
   """
     Visualization
   """
-  # TODO: visualization part
-  # remember to comment import when submitting in Autolab
-  # from matplotlib import pyplot as plt
-  # from skimage.io import imshow
-  #
-  # # output of layer 1
+  # TODO: remember to comment import when submitting in Autolab
+
+  from matplotlib import pyplot as plt
+  from skimage.io import imshow
+  import matplotlib.gridspec as gridspec
+
+  # output of layer 1
   # print output[1]['data'].shape     # (784, 64)
-  # h1 = output[1]['height']
-  # w1 = output[1]['width']
-  # plt.imshow(output[1]['data'][:, 1].reshape((h, w)))
-  # plt.show()
-  #
-  # # output of layer 2
-  # print 'output layer 2', output[2]['data'].shape     # (11520 = 24*24*20, 64)
-  # h2 = output[2]['height']
-  # w2 = output[2]['width']
-  #
-  # # output of layer 3
-  # print 'output layer 3', output[3]['data'].shape       # (2880 = 12*12*20, 64)
+  fig1 = plt.figure(1)
+  plt.title('Original Image')
+  h1 = output[1]['height']
+  w1 = output[1]['width']
+  plt.imshow(output[1]['data'][:, 1].reshape((h1, w1)))
+  plt.axis('off')
+
+  # output of layer 2
+  print 'output layer 2', output[2]['data'].shape     # (11520 = 24*24*20, 64)
+  h2 = output[2]['height']
+  w2 = output[2]['width']
+  c = output[2]['channel']
+  output_data = np.reshape(output[2]['data'][:, 1], (h2, w2, c))
+
+  fig2 = plt.figure(2)
+  fig2.suptitle("Output of Layer 2")
+  gs = gridspec.GridSpec(4, 5)
+  channel = 0
+  # ax = plt.subplot(gs[1, 1])
+  # ax.imshow(output_data[:,:, channel])
+  # fig.add_subplot(ax)
+  for i in range(4):
+      for j in range(5):
+          ax = plt.subplot(gs[i, j])
+          plt.axis('off')
+          ax.imshow(output_data[:,:, channel])
+          fig2.add_subplot(ax)
+          channel += 1
+
+  plt.show()
+  plt.close(fig1)
+  plt.close(fig2)
+
+
+
+  # output of layer 3
+  print 'output layer 3', output[3]['data'].shape       # (2880 = 12*12*20, 64)
 
   return cp, param_grad
 
